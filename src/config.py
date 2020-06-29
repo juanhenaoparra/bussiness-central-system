f2e816bcaa3eea1e0c389783441b443e7f9704fc
Juan Henao <parjuag@gmail.com>
2020-06-29 09:10:12 -0500

    Indent error

--- src/config.py
+++ src/config.py
@@ -2,19 +2,19 @@ from configparser import ConfigParser
 
 
 class Singleton(type):
-    """
-    Define an Instance operation that lets clients access its unique
-    instance.
-    """
+  """
+  Define an Instance operation that lets clients access its unique
+  instance.
+  """
 
-    def __init__(cls, name, bases, attrs, **kwargs):
-        super().__init__(name, bases, attrs)
-        cls._instance = None
+  def __init__(cls, name, bases, attrs, **kwargs):
+    super().__init__(name, bases, attrs)
+    cls._instance = None
 
-    def __call__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__call__(*args, **kwargs)
-        return cls._instance
+  def __call__(cls, *args, **kwargs):
+    if cls._instance is None:
+      cls._instance = super().__call__(*args, **kwargs)
+    return cls._instance
 
 class Config(metaclass=Singleton):
   def __init__(self, path:str='assets/config.ini'):
